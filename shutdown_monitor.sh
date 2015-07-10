59661317714e696caed6341e13ede8aba019b3f4
Jonathon Wilson <chilkari@gmail.com>
2015-07-10 05:00:42 +0000

    Tweaks for shutdown monitor

--- shutdown_monitor.sh
+++ shutdown_monitor.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# FIXME - We set this up to run from ~/.profile, which means it tries to
+# run on any login. SSH'ing in will run it again (in addition to the one
+# that runs on boot/auto-login). Modify this script so that if there
+# is already a process, then bail out early.
+# We should be able to simply grep the process table for shutdown_monitor
+
 sfile="/home/pi/rpi-music/shutdown_request"
 # Upon booting, we don't immediately want to honor a shutdown request
 if  [ -f $sfile ];
