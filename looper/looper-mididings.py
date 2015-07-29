29d51bb2c1518f6189eebe1e2ea4808e9bd652be
Jonathon Wilson <chilkari@gmail.com>
2015-07-29 00:59:28 +0000

    OSC for SL reload. Lower latency, use auto-latency. Shutdown to 3 marker buttons only. Track left for reload.

--- looper/looper-mididings.py
+++ looper/looper-mididings.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python
 
+import sys
 import os
 from mididings import *
 from mididings.extra import *
+from mididings.extra.osc import SendOSC
+
+import liblo
 
 # some classes are defined in separate modules because they depend on
 # additional Python packages to be installed. uncomment these imports if you
 # need them
 #from mididings.extra.osc import OSCInterface
 #from mididings.extra.inotify import AutoRestart
+
+# Overview/Notes
+# Control SooperLooper from a NanoKontrol2
+# track left/right, marker set, left, right have no lights, and are used
+# as 'function' keys.
+
+SLPORT = 9951
 
 config(
     in_ports=['NK2_IN'],
@@ -35,30 +46,24 @@ config(
 # kick of lighting of status lights.
 
 
-# To request a shutdown, user must hold all three of the
-# 8th track's record, mute and solo buttons down. These
-# flags store each state
+# To request a shutdown, user must hold set, left, right marker buttons together.
 shutdown_a = False
 shutdown_b = False
 shutdown_c = False
-shutdown_d = False
 
 def part_of_shutdown(ev):
-    print "part_of_shutdown"
+    # print "part_of_shutdown"
     global shutdown_a
     global shutdown_b
     global shutdown_c
-    global shutdown_d
     if ev.type == NOTEON:
-        if ev.note == 0:
-            shutdown_a = True
         if ev.note == 6:
-            shutdown_b = True
+            shutdown_a = True
         if ev.note == 7:
-            shutdown_c = True
+            shutdown_b = True
         if ev.note == 8:
-            shutdown_d = True
-        if shutdown_a and shutdown_b and shutdown_c and shutdown_d:
+            shutdown_c = True
+        if shutdown_a and shutdown_b and shutdown_c
             # TODO/FIXME - blink transport lights indicating shutdown
             # Ideally, rpi-audio stop will turn off 'power' light when done stopping
             # This won't work. I'm thinking I'll need to have a script running as root
@@ -67,24 +72,32 @@ def part_of_shutdown(ev):
             # then shuts down. I would write that file here.
             # Better approach if its running: use dbus to communicate from here to
             # the running-as-root shutdown script.
-            print "SHUTDOWN REQUESTED. TODO - implement me."
             with open('/home/pi/rpi-music/shutdown_request', 'w') as f:
                 f.write('shutdown, please!')
 
     if ev.type == NOTEOFF:
-        if ev.note == 0:
-            shutdown_a = False
         if ev.note == 6:
-            shutdown_b = False
+            shutdown_a = False
         if ev.note == 7:
-            shutdown_c = False
+            shutdown_b = False
         if ev.note == 8:
-            shutdown_d = False
+            shutdown_c = False
 
+def reset(ev):
+    # Straight liblo here for OSC
+    try:
+        # ping for testing...
+        # liblo.send(SLPORT, "/ping", "osc.udp://127.0.0.1:9999", "/pong")
+        # Pause the loops
+        liblo.send(SLPORT, "/sl/-1/hit", "pause")
+        liblo.send(SLPORT, "/load_session", "/home/pi/rpi-music/looper/session.slsess", "osc.udp://127.0.0.1:9999", "/load_errors")
+    except:
+        print sys.exc_info()
 
 scenes = {
     1: Scene("looper", [
         Channel(13) >> Port('NK2_OUT'),
+        Filter(NOTEON) >> KeyFilter(notes=[9]) >> Process(reset),
         KeyFilter(notes=[0,6,7,8]) >> Process(part_of_shutdown),
         Filter(NOTE) >> Port('SL'),
         Filter(CTRL) >> Port('SL'),
