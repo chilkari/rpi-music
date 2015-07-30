4bd3eb4c4832e6059b7e9985f3fd167610116822
Jonathon Wilson <chilkari@gmail.com>
2015-07-30 01:19:31 +0000

    Hmmm. Bring in disable-ethernet script from internet. Comment out shutdown running check with fixme

--- shutdown_monitor.sh
+++ shutdown_monitor.sh
@@ -10,11 +10,12 @@ MACHINE_TYPE=looper
 
 # First, see if shutdown_monitor is already running. (This happens if you
 # boot the RPi, then login from a console, which tries to run it again)
-RUNNING=`ps -ef | sed -n /sudo.*[s]hutdown_monitor.sh/p`
-if [ "${RUNNING:-null}" != null ]; then
-    echo "Shutdown monitor already running. Aborting..."
-    exit 1
-fi
+echo "FIXME - NEED TO REPAIR CHECK FOR RUNNING IN SHUTDOWN MONITOR. IF LOGGING IN _ PLEASE KILL EXTRA PROCS"
+# RUNNING=`ps -ef | sed -n /sudo.*[s]hutdown_monitor.sh/p`
+# if [ "${RUNNING:-null}" != null ]; then
+#     echo "Shutdown monitor already running. Aborting..."
+#     exit 1
+# fi
 
 sfile="/home/pi/rpi-music/shutdown_request"
 # Upon booting, we don't immediately want to honor a shutdown request
